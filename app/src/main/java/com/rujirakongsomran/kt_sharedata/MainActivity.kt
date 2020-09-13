32b3d89fe911673e1678e6001a17a9c3c51a92a9
RujiraKongsomran <rujirabiwberry@gmail.com>
2020-09-13 14:45:41 +0700

    finish

--- app/src/main/java/com/rujirakongsomran/kt_sharedata/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/kt_sharedata/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.rujirakongsomran.kt_sharedata
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.share_menu) {
+            val shareIntent = Intent().apply {
+                this.action = Intent.ACTION_SEND
+                this.putExtra(Intent.EXTRA_TEXT, "We are sharing data between apps")
+                this.type = "text/plain"
+            }
+            startActivity(shareIntent)
+
+        } else {
+            return super.onOptionsItemSelected(item)
+        }
+        return true
+
     }
 }
